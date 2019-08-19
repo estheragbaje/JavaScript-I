8b280ec45854ad012bd0b6844c2944039fc5db2c
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-08-19 22:52:18 +0100

    Function Conversion Exercise + Stretch Goal Completed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,37 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+  console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = param => {
+  return param;
+};
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+  return param1 + param2;
+};
+add(1, 2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1 - param2;
 
 // Stretch
 
@@ -28,3 +44,11 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+// let func = function(num){
+//     return num * 3
+// }
+
+let exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map(num => num * 3);
+console.log({ input: exampleArray, output: triple });
